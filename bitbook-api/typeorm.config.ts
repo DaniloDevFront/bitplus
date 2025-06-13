365809e7a6fac7f443e14b4cc8ac0f340c8962be
DaniloBitFront <danilo.engenharia@bithealth.com.br>
2025-06-13 11:22:12 -0300

    create config typeorm

---
+++ bitbook-api/typeorm.config.ts
@@ -0,0 +1,13 @@
+import { DataSource } from 'typeorm';
+
+export default new DataSource({
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/migrations/*.js'],
+  synchronize: false,
+});
