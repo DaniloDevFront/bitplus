6e62eeea0f9e871b34a06f0eb6d5f0d98f7e9c96
EC2 Default User <ec2-user@ip-172-31-11-49.sa-east-1.compute.internal>
2025-06-17 11:20:59 -0300

    git push

---
+++ backups/backup_bitbook-api_homolog_20250617_123011.sql
@@ -0,0 +1,477 @@
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19  Distrib 10.5.29-MariaDB, for Linux (x86_64)
+--
+-- Host: bitplus.c74yig0isli4.sa-east-1.rds.amazonaws.com    Database: bitbook_homolog
+-- ------------------------------------------------------
+-- Server version	8.0.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `banner_providers`
+--
+
+DROP TABLE IF EXISTS `banner_providers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `banner_providers` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `banner_id` int NOT NULL,
+  `provider_id` int NOT NULL,
+  `provider_name` varchar(255) DEFAULT 'provedor -',
+  PRIMARY KEY (`id`),
+  KEY `FK_d8cea7efda72270b699707c02db` (`banner_id`),
+  CONSTRAINT `FK_d8cea7efda72270b699707c02db` FOREIGN KEY (`banner_id`) REFERENCES `banners` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `banner_providers`
+--
+
+LOCK TABLES `banner_providers` WRITE;
+/*!40000 ALTER TABLE `banner_providers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `banner_providers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `banners`
+--
+
+DROP TABLE IF EXISTS `banners`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `banners` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `banner` varchar(255) NOT NULL,
+  `premium` tinyint NOT NULL DEFAULT '0',
+  `action_internal` varchar(255) DEFAULT NULL,
+  `action_external` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `banners`
+--
+
+LOCK TABLES `banners` WRITE;
+/*!40000 ALTER TABLE `banners` DISABLE KEYS */;
+INSERT INTO `banners` VALUES (1,'https://app-bittrainers-v2.s3.sa-east-1.amazonaws.com/bannersTeste/e0dbc2b9-7eed-4081-93f4-616f200fb31d.png',0,'/teste','');
+/*!40000 ALTER TABLE `banners` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bookcases`
+--
+
+DROP TABLE IF EXISTS `bookcases`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bookcases` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `book_id` int NOT NULL,
+  `audiobook_id` int DEFAULT NULL,
+  `status` tinyint NOT NULL DEFAULT '1',
+  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  PRIMARY KEY (`id`),
+  KEY `FK_c00b2021d7f1421345471169292` (`user_id`),
+  KEY `FK_91ea58383c568b418ce8264f8c7` (`book_id`),
+  CONSTRAINT `FK_91ea58383c568b418ce8264f8c7` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`),
+  CONSTRAINT `FK_c00b2021d7f1421345471169292` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bookcases`
+--
+
+LOCK TABLES `bookcases` WRITE;
+/*!40000 ALTER TABLE `bookcases` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bookcases` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `books`
+--
+
+DROP TABLE IF EXISTS `books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `books` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `premium` tinyint NOT NULL DEFAULT '0',
+  `high` tinyint NOT NULL DEFAULT '0',
+  `type` enum('ebook','audiobook') NOT NULL DEFAULT 'ebook',
+  `description` text,
+  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  `category_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_46f5b35b90175a660f99810bc97` (`category_id`),
+  CONSTRAINT `FK_46f5b35b90175a660f99810bc97` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `books`
+--
+
+LOCK TABLES `books` WRITE;
+/*!40000 ALTER TABLE `books` DISABLE KEYS */;
+INSERT INTO `books` VALUES (1,'Ebook Demo',0,1,'ebook','Só tem uma forma de sobreviver às altas temperaturas do verão: apostando em um cardápio leve, natural e hiper-refrescante! Afinal, a estação mais quente do ano pede uma ajudinha constante ao organismo.','2025-06-16 19:14:48.645107','2025-06-16 19:14:48.645107',1);
+/*!40000 ALTER TABLE `books` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `books_media`
+--
+
+DROP TABLE IF EXISTS `books_media`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `books_media` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `file_url` varchar(255) DEFAULT NULL,
+  `img_small` varchar(255) DEFAULT NULL,
+  `img_medium` varchar(255) DEFAULT NULL,
+  `img_large` varchar(255) DEFAULT NULL,
+  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  `books_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `REL_cda162d88534fe29f5b853a1f8` (`books_id`),
+  CONSTRAINT `FK_cda162d88534fe29f5b853a1f80` FOREIGN KEY (`books_id`) REFERENCES `books` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `books_media`
+--
+
+LOCK TABLES `books_media` WRITE;
+/*!40000 ALTER TABLE `books_media` DISABLE KEYS */;
+INSERT INTO `books_media` VALUES (1,'https://app-bittrainers-v2.s3.sa-east-1.amazonaws.com/books/files/57cd8a49-7112-46a7-bf4b-8699ad07699c.pdf','https://app-bittrainers-v2.s3.sa-east-1.amazonaws.com/books/covers/1750101288513-small.jpg','https://app-bittrainers-v2.s3.sa-east-1.amazonaws.com/books/covers/1750101288513-medium.jpg','https://app-bittrainers-v2.s3.sa-east-1.amazonaws.com/books/covers/0e141182-fb34-4e2c-bff4-28d4303ce30c.png','2025-06-16 19:14:48.652738','2025-06-16 19:14:48.652738',1);
+/*!40000 ALTER TABLE `books_media` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `books_sheets`
+--
+
+DROP TABLE IF EXISTS `books_sheets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `books_sheets` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `author` varchar(255) NOT NULL,
+  `genre` varchar(255) NOT NULL,
+  `year` int NOT NULL,
+  `ref` varchar(255) NOT NULL,
+  `books_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `REL_5139deff6403868f55adc51723` (`books_id`),
+  CONSTRAINT `FK_5139deff6403868f55adc51723b` FOREIGN KEY (`books_id`) REFERENCES `books` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `books_sheets`
+--
+
+LOCK TABLES `books_sheets` WRITE;
+/*!40000 ALTER TABLE `books_sheets` DISABLE KEYS */;
+INSERT INTO `books_sheets` VALUES (1,'Carla Ribeiro','Culinária',2021,'REF001',1);
+/*!40000 ALTER TABLE `books_sheets` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `books_tracks`
+--
+
+DROP TABLE IF EXISTS `books_tracks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `books_tracks` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `audio_url` varchar(255) NOT NULL,
+  `duration` varchar(255) NOT NULL DEFAULT '00:00:00',
+  `order` int NOT NULL DEFAULT '0',
+  `description` text,
+  `cover_small` text,
+  `cover_medium` text,
+  `cover_large` text,
+  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  `media_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_43a6f0c72f6eb0a910e6a3babc2` (`media_id`),
+  CONSTRAINT `FK_43a6f0c72f6eb0a910e6a3babc2` FOREIGN KEY (`media_id`) REFERENCES `books_media` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `books_tracks`
+--
+
+LOCK TABLES `books_tracks` WRITE;
+/*!40000 ALTER TABLE `books_tracks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `books_tracks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categories` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `ico` varchar(255) NOT NULL,
+  `premium` tinyint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'Receitas','https://app-bittrainers-v2.s3.sa-east-1.amazonaws.com/categories/icons/908dc17c-e59b-482f-9307-1e2085799a90.png',0);
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `migrations`
+--
+
+DROP TABLE IF EXISTS `migrations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `migrations` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `timestamp` bigint NOT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `migrations`
+--
+
+LOCK TABLES `migrations` WRITE;
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `profiles`
+--
+
+DROP TABLE IF EXISTS `profiles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `profiles` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `cpf` varchar(255) DEFAULT NULL,
+  `birth_date` date DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  `cover` varchar(255) DEFAULT NULL,
+  `provider_id` int DEFAULT NULL,
+  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `IDX_30d20cfa6967ffa3e3a9c55469` (`cpf`),
+  UNIQUE KEY `REL_9e432b7df0d182f8d292902d1a` (`user_id`),
+  CONSTRAINT `FK_9e432b7df0d182f8d292902d1a2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `profiles`
+--
+
+LOCK TABLES `profiles` WRITE;
+/*!40000 ALTER TABLE `profiles` DISABLE KEYS */;
+INSERT INTO `profiles` VALUES (1,'Danilo Andrade','(11) 99999-9999','137.945.596-06','1990-01-01',NULL,NULL,NULL,'2025-06-14 15:14:13.175034','2025-06-14 15:14:13.175034',1);
+/*!40000 ALTER TABLE `profiles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `providers`
+--
+
+DROP TABLE IF EXISTS `providers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `providers` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `providers`
+--
+
+LOCK TABLES `providers` WRITE;
+/*!40000 ALTER TABLE `providers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `providers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rate_history`
+--
+
+DROP TABLE IF EXISTS `rate_history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rate_history` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `rating` decimal(2,1) NOT NULL,
+  `comment` text,
+  `status` tinyint NOT NULL DEFAULT '1',
+  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `rate_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_f80fe41c2ba50d0892f6f105d58` (`rate_id`),
+  CONSTRAINT `FK_f80fe41c2ba50d0892f6f105d58` FOREIGN KEY (`rate_id`) REFERENCES `rates` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rate_history`
+--
+
+LOCK TABLES `rate_history` WRITE;
+/*!40000 ALTER TABLE `rate_history` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rate_history` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rates`
+--
+
+DROP TABLE IF EXISTS `rates`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rates` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_89adf0a54b6daa47d96cb435878` (`user_id`),
+  CONSTRAINT `FK_89adf0a54b6daa47d96cb435878` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rates`
+--
+
+LOCK TABLES `rates` WRITE;
+/*!40000 ALTER TABLE `rates` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rates` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `readings`
+--
+
+DROP TABLE IF EXISTS `readings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `readings` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `current_page` int NOT NULL DEFAULT '0',
+  `progress` float NOT NULL DEFAULT '0',
+  `status` tinyint NOT NULL DEFAULT '1',
+  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  `user_id` int DEFAULT NULL,
+  `book_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_0e3a7a8ef0c7f9ad758f4bc0e94` (`user_id`),
+  KEY `FK_7fb6640d22c7483fa51cc39269a` (`book_id`),
+  CONSTRAINT `FK_0e3a7a8ef0c7f9ad758f4bc0e94` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `FK_7fb6640d22c7483fa51cc39269a` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `readings`
+--
+
+LOCK TABLES `readings` WRITE;
+/*!40000 ALTER TABLE `readings` DISABLE KEYS */;
+/*!40000 ALTER TABLE `readings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `premium` tinyint NOT NULL DEFAULT '0',
+  `role` varchar(255) NOT NULL DEFAULT 'client',
+  `terms` tinyint NOT NULL DEFAULT '0',
+  `biometricSecretHash` varchar(255) DEFAULT NULL,
+  `isBiometricEnabled` tinyint NOT NULL DEFAULT '0',
+  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `IDX_97672ac88f789774dd47f7c8be` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'danilo.devfront@gmail.com','$2b$10$2sKhErkXXnwadNjT5dfyX.SS.h1iCijnExIHCkEJpBSIber4pK8OC',0,'admin',1,NULL,0,'2025-06-14 15:14:13.166595','2025-06-16 17:33:41.196905');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-06-17  9:30:13
